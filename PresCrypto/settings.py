ce2c5223123d36fed9611ebe353d3b16769930bb
Mr-Destructive <gormeet711@gmail.com>
2022-04-10 07:26:27 +0530

    heroku deployment changes

--- PresCrypto/settings.py
+++ PresCrypto/settings.py
@@ -1,14 +1,18 @@
 from pathlib import Path
 import os
+import dj_database_url
+import django_heroku
+from pathlib import Path
+
+import os
+from dotenv import load_dotenv
 
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-
-SECRET_KEY = "django-insecure-@*1_v$2!y#px7_pv)f0@!+80oh^a)ji6_^0%4m7##xrpzhkr3o"
-
+load_dotenv(os.path.join(BASE_DIR, ".env"))
+SECRET_KEY = os.getenv("SECRET_KEY")
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["herokuapp.com", "127.0.0.1", "localhost:8000"]
 
 
 INSTALLED_APPS = [
@@ -94,6 +98,7 @@ DATABASES = {
     }
 }
 
+django_heroku.settings(locals())
 STATIC_URL = "static/"
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn PresCrypto.wsgi
